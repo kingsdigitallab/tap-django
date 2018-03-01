451784c4196a39b1520a99d79fe3d0f14cd5a3d4
Miguel Vieira <jmvieira@gmail.com>
2018-03-01 14:53:25 +0000

    Adds a new field to the documents being stored in mongo, that contains all the words in a tweet.

--- chirp/twitter.py
+++ chirp/twitter.py
@@ -1,11 +1,14 @@
 import logging
+import re
+import string
 
 import requests
 import simplejson
 from django.conf import settings as s
+from nltk.corpus import stopwords
 from requests_oauthlib import OAuth1
 from simplejson.errors import JSONDecodeError
-from textblob import TextBlob
+from textblob import TextBlob, Word
 
 logger = logging.getLogger(__name__)
 
@@ -29,9 +32,10 @@ def _process_tweets(f, collection):
 
         t['chirp'] = {}
 
-        t = _add_sentiment_analysis_to_tweet(t)
+        t = _add_npl_to_tweet(t)
 
-        collection.insert(t)
+        if not collection.find_one({'id_str': t['id_str']}):
+            collection.insert(t)
 
 
 def tweet_generator(follow='', track='', locations=''):
@@ -62,14 +66,114 @@ def tweet_generator(follow='', track='', locations=''):
                 logger.error(e.args)
 
 
-def _add_sentiment_analysis_to_tweet(tweet):
-    if 'text' not in tweet:
+def _add_npl_to_tweet(tweet):
+    text = get_text_from_tweet(tweet)
+
+    if not text:
         return tweet
 
-    textblob = TextBlob(tweet['text'])
-    sentiment = textblob.sentiment
+    sentiment = get_sentiment_from_tweet_text(text)
 
-    tweet['chirp']['sentiment'] = {}
-    tweet['chirp']['sentiment']['polarity'] = sentiment.polarity
-    tweet['chirp']['sentiment']['subjectivity'] = sentiment.subjectivity
+    if sentiment:
+        tweet['chirp']['sentiment'] = {}
+        tweet['chirp']['sentiment']['polarity'] = sentiment.polarity
+        tweet['chirp']['sentiment']['subjectivity'] = sentiment.subjectivity
+
+    words = get_words_from_tweet_text(text)
+
+    if words:
+        tweet['chirp']['words'] = words
+
     return tweet
+
+
+def get_text_from_tweet(tweet):
+    if 'text' not in tweet:
+        return None
+
+    text = tweet['text']
+
+    if 'retweeted_status' in tweet:
+        if 'extended_tweet' in tweet['retweeted_status']:
+            text = tweet['retweeted_status']['extended_tweet']['full_text']
+        else:
+            text = tweet['retweeted_status']['text']
+
+    return text
+
+
+def get_sentiment_from_tweet_text(text):
+    textblob = TextBlob(text)
+    return textblob.sentiment
+
+
+def get_words_from_tweet_text(text):
+    tokens = _tokenize(text)
+
+    words = [
+        token if _is_emoticon(token) else _get_lemma(token)
+        for token in tokens
+        if _is_valid_token(token)
+    ]
+
+    return words
+
+
+emoticon_str = r"""(?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+emoticon_re = re.compile(r'^{}$'.format(emoticon_str),
+                         re.VERBOSE | re.IGNORECASE)
+
+
+tokens_str = [
+    emoticon_str,
+    r'<[^>]+>',  # HTML tags
+    r'(?:@[\w_]+)',  # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)",  # hash-tags
+    # URLs
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+',  # noqa
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)',  # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])",  # words with - and '
+    r'(?:[\w_]+)',  # other words
+    r'(?:\S)'  # anything else
+]
+tokens_re = re.compile(r'({})'.format('|'.join(tokens_str)),
+                       re.VERBOSE | re.IGNORECASE)
+
+
+def _tokenize(text):
+    return tokens_re.findall(text)
+
+
+def _is_emoticon(token):
+    if emoticon_re.search(token):
+        return True
+
+    return False
+
+
+def _get_lemma(token):
+    return Word(token.lower()).lemmatize()
+
+
+stop = stopwords.words('english')
+stop.append('amp')
+
+punctuation = string.punctuation
+punctuation = '{}’“”…'.format(punctuation)
+
+
+def _is_valid_token(token):
+    if not token:
+        return False
+
+    token = token.lower()
+
+    if token in stop:
+        return False
+    if token in punctuation:
+        return False
+    return True
