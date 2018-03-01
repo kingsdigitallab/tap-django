688b5d126e321643ba80600cbbf6c06be3d9fc50
Miguel Vieira <jmvieira@gmail.com>
2018-03-01 14:51:24 +0000

    Added views to get tweets, get words. The filter detail view now checks for query parameters.

--- chirp/views.py
+++ chirp/views.py
@@ -1,3 +1,7 @@
+import json
+import re
+
+from bson import json_util
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
@@ -12,6 +16,20 @@ class FilterDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        query = None
+        context['query'] = query
+
+        if 'query-field' in self.request.GET:
+            if 'query-value' in self.request.GET:
+                regex = re.compile(self.request.GET['query-value'])
+                query = {self.request.GET['query-field']: regex}
+
+                context['query'] = query
+
+        f = Filter.objects.get(id=self.object.id)
+        context['tweets'] = f.get_tweets(query=query)
+
         return context
 
 
@@ -24,6 +42,31 @@ def get_filters(request):
     filters = [{'id': f.id, 'label': f.label, 'active': f.active}
                for f in Filter.objects.all()]
     return Response(filters)
+
+
+@api_view(['GET'])
+def get_tweets(request, filter_id):
+    f = Filter.objects.get(id=filter_id)
+    query = None
+
+    if 'query-field' in request.GET:
+        if 'query-value' in request.GET:
+            query = {}
+            query = {request.GET['query-field']: request.GET['query-value']}
+
+    tweets = [
+        json.loads(json.dumps(item, indent=4, default=json_util.default))
+        for item in f.get_tweets(query=query)
+    ]
+
+    return Response(tweets)
+
+
+@api_view(['GET'])
+def get_words(request, filter_id):
+    f = Filter.objects.get(id=filter_id)
+
+    return Response(f.get_words())
 
 
 @api_view(['GET'])
