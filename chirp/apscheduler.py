a545d3bf16a320a7bac9a5567c786d9d2536d4ad
Miguel Vieira <jmvieira@gmail.com>
2018-03-13 10:58:29 +0000

    Removed print statement.

--- chirp/apscheduler.py
+++ chirp/apscheduler.py
@@ -16,14 +16,11 @@ scheduler.start()
 
 @receiver(post_save, sender=Filter, dispatch_uid="add_remove_job")
 def add_remove_jobs(sender, instance, **kwargs):
-    print("post_save")
     if instance.active:
         if instance.user.has_twitter_credentials:
-            scheduler.add_job(harvest, 'interval',
-                              args=[instance],
-                              id=instance.uid,
-                              minutes=1, max_instances=1,
-                              replace_existing=True)
+            scheduler.add_job(
+                harvest, 'interval', args=[instance], id=instance.uid,
+                minutes=1, max_instances=1, replace_existing=True)
     else:
         try:
             scheduler.remove_job(instance.uid)
