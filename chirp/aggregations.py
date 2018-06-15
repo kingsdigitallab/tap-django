5f0a0b5e50bb61be73c8561d7b31eb450a72b7e9
Miguel Vieira <jmvieira@gmail.com>
2018-06-15 11:57:27 +0100

    Added aggregation to return the top 200 words.

--- chirp/aggregations.py
+++ chirp/aggregations.py
@@ -6,3 +6,32 @@ sentiment_avg = [{
         }
     }
 }]
+
+words_top_200 = [
+    {
+        "$unwind": "$chirp.words"
+    },
+    {
+        "$project": {
+            "word": {
+                "$toLower": "$chirp.words"
+            }
+        }
+    },
+    {
+        "$group": {
+            "_id": "$word",
+            "value": {
+                "$sum": 1
+            }
+        }
+    },
+    {
+        "$sort": {
+            "value": -1
+        }
+    },
+    {
+        "$limit": 200
+    }
+]
